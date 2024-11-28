3c4f6814fb12e3f5ab2ab817e97368a2aaed6049
matheus itix-pc <mtknack555@gmail.com>
2024-11-28 15:05:48 -0300

    feat: ajustando arquivos

--- aula-28-11.pl
+++ aula-28-11.pl
@@ -1,72 +1,24 @@
-% Pessoas:
-pessoa(andre, 16).
-pessoa(bruno, 11).
-pessoa(caio, 19).
-pessoa(caio, 16).
-pessoa(catterina, 17).
-pessoa(diogo, 10).
-pessoa(erik, 11).
-pessoa(felipe, 17).
-pessoa(filipe, 14).
-pessoa(gabriel, 15).
-pessoa(gustavo, 16).
-pessoa(kaio, 11).
-pessoa(marcelo, 13).
-pessoa(matheus, 12).
-pessoa(pedro, 13).
-pessoa(ruan, 16).
-pessoa(talles, 14).
-pessoa(tiago, 14).
-pessoa(vinicius, 15).
-
-pessoa(P) :- pessoa(P, _).
-
-% LISTA O NOME DE TODAS AS PESSOAS
-listaPessoas(L) :-
-    listaPessoas([], L), !.
-
-listaPessoas(Aux, L) :-
-    pessoa(N), naoMembro(N, Aux),
-    listaPessoas([N|Aux], L).
-listaPessoas(Aux, L) :- L = Aux.
-
-% LISTA O NOME E A IDADE DE TODAS AS PESSOAS
-listaPessoas2(L) :- listaPessoas2([], L).
-listaPessoas2(Aux, L) :-
-    pessoa(P, N),
-    Item = [P,N],
-    naoMembro(Item, Aux),
-    listaPessoas2([Item|Aux], L).
-listaPessoas2(Aux, L) :- L = Aux.
-
-% VERIFICA SE ALGUM ITEM JÁ EXISTE EM UMA OUTRA LISTA
-naoMembro(_, []) :- !.
-naoMembro(E, [E|_]) :- !, fail.
-naoMembro(E, [_|Resto]) :-
-    naoMembro(E, Resto).
-
-
-% ENCONTRAR O MENOR ITEM DA LISTA
-num(2).
-num(1).
-num(3).
-
-menor(Menor) :- menor([], 999999, Menor).
-menor(ListaAux, MenorAux, Menor) :- num(N), naoMembro(N, ListaAux), N < MenorAux, menor([N|ListaAux], N, Menor).
-menor(ListaAux, MenorAux, Menor) :- num(N), naoMembro(N, ListaAux), N >= MenorAux, menor([N|ListaAux], MenorAux, Menor).
-menor(_,MenorAux, Menor) :- Menor = MenorAux.
-
-% encontrar a pessoa com a menor idade
-menorPessoa(ListaAux, MenorAux, Menor) :-
-    pessoa(Nome, Numero),
-    naoMembro(Numero, ListaAux),
-    MenorAux = [P_Aux, N_Aux],
-    Numero < N_Aux,
-    menorPessoa([N_Aux|ListaAux], [Nome, Numero], Menor).
-menorPessoa(ListaAux, MenorAux, Menor) :-
-    pessoa(Nome, Numero),
-    naoMembro(Numero, ListaAux),
-    MenorAux = [P_Aux, N_Aux],
-    Numero >= N_Aux,
-    menorPessoa([Numero|ListaAux], MenorAux, Menor).
-menorPessoa(_, MenorAux, Menor) :- Menor = MenorAux.
+conexao(a, b).
+conexao(a, d).
+conexao(a, f).
+conexao(a, m).
+conexao(b, c).
+conexao(b, e).
+conexao(b, v).
+conexao(d, i).
+conexao(d, j).
+conexao(f, g).
+conexao(f, h).
+conexao(m, n).
+conexao(m, o).
+conexao(i, y).
+conexao(j, k).
+conexao(j, l).
+conexao(n, q).
+conexao(n, p).
+conexao(k, t).
+conexao(k, u).
+conexao(q, r).
+conexao(q, s).
+conexao(r, w).
+conexao(s, x).
--- exercicios-fixação.pl
+++ exercicios-fixação.pl
@@ -4,5 +4,29 @@ pai( alfredo, joana ).
 pai( jose, alfredo ).
 casado( alfredo, ingred ).
 mae(Mae, Filho) :- pai(Pai, Filho), casado(Pai, Mae).
-irmao(Filho1, Filho2) :- pai(Pai, Filho1) = pai(Pai, Filho2).
+irmao(Filho1, Filho2) :- pai(Pai, Filho1), pai(Pai, Filho2).
 
+% irmao feito pelo professor
+naoMembro(_, []) :- !.
+naoMembro(Elem, [A|B]) :- Elem \= A, naoMembro(Elem, B).
+
+irmao(Pai, ListaAux, Irmaos) :- pai(Pai, I), naoMembro(I, ListaAux), irmao(Pai, [I|ListaAux], Irmaos).
+% falta terminar
+
+% 6
+membro([Elem|_], Elem).
+membro([_|L2], Elem) :- membro(L2, Elem).
+
+% 7
+itemDaLista([Elem|_], Elem, N) :- N is 0.
+itemDaLista([_|L2], Elem, N) :- itemDaLista(L2, Elem, NAux), N is NAux + 1.
+
+% 8
+contarElemDaLista([Elem|_], Elem, 0).
+contarElemDaLista([_|R], Elem, Qtd) :- contarElemDaLista(R, Elem, QtdAux), Qtd is QtdAux + 1.
+
+% 9
+consecutivosNaLista([Elem,Elem|Resto], Elem).
+consecutivosNaLista([_|Resto], Elem) :- consecutivosNaLista(Resto, Elem).
+
+% 10
